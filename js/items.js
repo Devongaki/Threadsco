9d8dd6f229d1f8036f00c77aea823706c7a551b8
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-06-02 07:06:44 +0200

    Create items.js

---
+++ js/items.js
@@ -0,0 +1,21 @@
+const productItem = [
+    {
+        id: 1,
+        title: 'Item 1',
+        image: 'item1.jpg',
+        description: 'This is item 1',
+        oldPrice: 10.99,
+        newPrice: 8.99
+    },
+
+    {
+        id: 2,
+        title: 'Item 2',
+        image: 'item1.jpg',
+        description: 'This is item 1',
+        oldPrice: 10.99,
+        newPrice: 8.99
+    }
+]
+
+export default productItem;
