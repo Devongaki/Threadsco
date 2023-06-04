cc8e2dbcf48a289d0b7f553f919f5d294205bf7e
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-06-04 11:23:44 +0200

    n

--- js/items.js
+++ js/items.js
@@ -1,21 +1,71 @@
 const items = [
-    {
-        id: 1,
-        title: 'Item 1',
-        image: 'images/men/men 1.jpeg',
-        description: 'This is item 1',
-        oldPrice: 10.99,
-        newPrice: 8.99
-    },
+  {
+    id: 1,
+    title: "Green Dress",
+    image: "images/homepage/sale 1.jpeg",
+    description: "This is item 1",
+    oldPrice: 680.99,
+    newPrice: 500.99,
+  },
 
-    {
-        id: 2,
-        title: 'Item 2',
-        image: 'item1.jpg',
-        description: 'This is item 1',
-        oldPrice: 10.99,
-        newPrice: 8.99
-    }
-]
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 2.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 3.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 4.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 5.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 6.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 7.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+  {
+    id: 2,
+    title: "Denim Jacket",
+    image: "images/homepage/sale 8.jpeg",
+    description: "This is item 1",
+    oldPrice: 590.99,
+    newPrice: 380.99,
+  },
+];
 
 export default items;
