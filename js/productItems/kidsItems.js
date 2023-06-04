2d9d45fa53eb7a337bc4df0b9b8159384f2a61c8
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-06-04 12:58:46 +0200

    n

---
+++ js/productItems/kidsItems.js
@@ -0,0 +1,12 @@
+const kidsItems = [
+    {
+    id: 4,
+    title: "Grey Sweatshirt",
+    image: "images/homepage/sale 4.jpeg",
+    description: "This is item 1",
+    Price: 390.99,
+    type: "Kids collection",
+  },
+]
+
+export default kidsItems;
---
+++ js/productItems/womensItems.js
@@ -0,0 +1,94 @@
+const womensItems = [
+  {
+    id: 001,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 002,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 003,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 004,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 005,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 006,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 007,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 0001,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 0010,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+  {
+    id: 0011,
+    title: "Green Dress",
+    image: "images/woman/w 1.jpeg",
+    description:
+      "Elevate your style with this stunning green dress adorned with white stripes. The vibrant green color adds a fresh and lively touch, while the crisp white stripes create a chic and timeless look. The dress features a flattering silhouette that enhances your figure, making it suitable for various occasions.",
+    Price: 680.99,
+    type: "womens collection",
+  },
+];
+
+export default womensItems;
