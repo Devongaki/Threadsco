82c502254ebdc733e8095ec205f301486abfdbd9
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-06-11 18:03:57 +0200

    n

--- js/index.js
+++ js/index.js
@@ -1,31 +1,35 @@
 // Grnerate items and display them to the Dom
+const postHtml = document.querySelector(".product-container");
+const url ="";
 
-import items from "./items.js";
+async function fetchPoata() {
+  const response = await fetch(url);
+  const post = await response.json();
 
-const productContainer = document.querySelector(".product-container");
+  console.log(post);
 
-
-function getItems(arg) {
-    for(let i = 0; i < items.length; i++){
-
-        productContainer.innerHTML += `
-            <div class="product-card">
-                        <a href="product.html?id=${items[i].id}">
+  const createPosts = (posts) => {
+    const postContent = posts.map((post) => {
+      return `
+                    <div class="product-card">
+                        <a href="product.html?id">
                             <div class="product-image">
-                                <img src="${items[i].image}" alt="" loading="eager">
+                                <img src="${post.featured_image}" alt="" loading="eager">
                             </div>    
                         </a>
                         <div class="product-title">
-                                <p>${items[i].title}</p>
+                                <p>${post.title.rendered}</p>
                             </div>
                             <div class="product-price">
                                 <div class="price">
-                                    <p>${items[i].Price} kr</p>
+                                    <p>${post.Price} kr</p>
                                 </div>
                             </div>
-                    </div>
-        `;
-        console.log(items[i])
-    }
+                    </div>`;
+    });
+    postHtml.innerHTML = postContent;
+  };
+  createPosts(post);
 }
-getItems(items)
+
+fetchPoata();
